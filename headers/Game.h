a3b1b1fcea8afac5392ba904fc0bed52d310c265
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-06-05 09:59:29 +0300

    Add functions

--- headers/Game.h
+++ headers/Game.h
@@ -3,12 +3,19 @@
 #include <vector>
 #include "Player.h"
 #include "Cell.h"
+#include <Common.h>
 
 class Game
 {
 public:
 	Game();
 	~Game();
+
+	void draw();
+	void update();
+
+	bool offBounds(const coordinates&);
+	bool isEmptyCell(const coordinates& coor);
 
 private:
 	std::vector<Player*> m_players;
